a0f6234e2fde7937e1bbbbcc442ceb9c54db607b
szelos <szelos@intellisoft.gr>
2022-02-01 10:39:33 +0200

    The date picker scripts were removed from the individual pages and added to the main layout with Greek support

--- CoralTimeAdmin/Models/DayTasks.cs
+++ CoralTimeAdmin/Models/DayTasks.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CoralTimeAdmin.Models
@@ -6,24 +7,42 @@ namespace CoralTimeAdmin.Models
     public class DayTasks : ViewModelBase
     {
         public int Id { get; set; }
+
         public string Project { get; set; }
+
         public int ProjectId { get; set; }
+
         public int TaskTypesId { get; set; }
+
         public string Task { get; set; }
+
         public string TaskDescription { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:d}")]
         public DateTime Date { get; set; }
+
         public string LastUpdateDate { get; set; }
+
         public string Description { get; set; }
+
         public int TimeActual { get; set; }
+
         public int TimeFrom { get; set; }
+
         public int TimeTo { get; set; }
+
         public string FromTime { get; set; }
+
         public string ToTime { get; set; }
+
         public int TimeTimerStart { get; set; }
+
         public string TimeStart { get; set; }
+
         public string Duration { get; set; }
 
         [NotMapped] public string EventStart { get; set; }
+
         [NotMapped] public string EventEnd { get; set; }
 
         public DayTasks PreviewsTask { get; set; }
